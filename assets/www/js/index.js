77530a55ddb37c3049a171f886ed196b04651887
Dave E <dave.git.2012@coolhandmook.com>
2013-02-25 22:37:03 +0000

    Fetch twitter feed proof of concept

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -1,49 +1,59 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    // Application Constructor
+
     initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
+
+    onFreshTweets: function(tweets)
+    {
+	$("#current").empty();
+	$.each(app.tweetsToCurrent(tweets), function(key, value)
+        {
+	    var text = key + " (since " + value.toLocaleDateString() + ")";
+	    $("<li/>").append(text).appendTo("#current");
+	});
+    },
+
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+    onDeviceReady: function()
+    {
+	var url="https://api.twitter.com/1/statuses/user_timeline.json?screen_name=TheBatTaps&trim_user=t&include_rts=false&count=100&callback=?";
+	//var url="js/sample.json";
+	$.getJSON(url, function(data)
+	{
+	    var tweets = [];
+	    $.each(data, function(index, item)
+	    {
+		tweets.push({text:item.text,date:item.created_at});
+	    });
+	    tweets.reverse(); // chronological order please
+	    app.onFreshTweets(tweets);
+	});
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    tweetsToCurrent: function(tweets)
+    {
+	var result = {};
+	tweets.forEach(function(tweet)
+        {
+	    var text = tweet.text.replace(/\s+/g, ' ');
+	    var matches = /OFF: (.*) ON: (.*)/.exec(text);
+	    if (matches !== null)
+	    {
+		var off = matches[1];
+		if (off in result)
+		{
+		    console.log("erasing "+off);
+		    delete result[off];
+		}
+		console.log("adding "+matches[2]);
+		result[matches[2]] = new Date(tweet.date);
+	    }
+	});
+	return result;
+    },
 
-        console.log('Received Event: ' + id);
-    }
 };
