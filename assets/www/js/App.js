74a678b6f8b859d178b6b30f1473ec80c11c1d18
Dave E <dave.git.2012@coolhandmook.com>
2013-02-27 23:10:49 +0000

    Rename and reorder functions in App

--- assets/www/js/App.js
+++ assets/www/js/App.js
@@ -3,25 +3,25 @@ function App(viewDiv, twitterScreenName)
     var app = this;
 
     this.view = new View(viewDiv);
-    this.view.itemRemoveClicked = function(item) { app.onItemRemoveClicked(item); };
+    this.view.itemRemoveClicked = function(item) { app.onViewItemRemoveClicked(item); };
 
     this.model = new Model();
-    this.model.itemsLoaded = function() { app.onItemsLoaded(); };
-    this.model.itemRemoved = function(item) { app.onItemRemoved(item); };
+    this.model.itemsLoaded = function() { app.onModelItemsLoaded(); };
+    this.model.itemRemoved = function(item) { app.onModelItemRemoved(item); };
     this.model.load(twitterScreenName);
 }
 
-App.prototype.onItemsLoaded = function()
-{
-    this.view.refresh(this.model.items);
-};
-
-App.prototype.onItemRemoveClicked = function(item)
+App.prototype.onViewItemRemoveClicked = function(item)
 {
     this.model.remove(item.name);
 };
 
-App.prototype.onItemRemoved = function(item)
+App.prototype.onModelItemsLoaded = function()
+{
+    this.view.refresh(this.model.items);
+};
+
+App.prototype.onModelItemRemoved = function(item)
 {
     this.view.remove(item);
 };
