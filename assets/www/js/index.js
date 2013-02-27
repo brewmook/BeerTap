591644ce23f87c54ea310500dc8e8ac602eddc43
Dave E <dave.git.2012@coolhandmook.com>
2013-02-27 19:18:09 +0000

    Extracted twitter code

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -15,18 +15,6 @@ function formatDate(date)
     return fields.join('/');
 }
 
-// Mock back end pretending to be twitter
-var twitter = {
-    timelineQuery: function(screenName) {
-        //return "https://api.twitter.com/1/statuses/user_timeline.json?screen_name="+screenName+"&trim_user=t&include_rts=false&count=100&callback=?";
-	return "js/sample.json";
-    },
-
-    tweet: function(text) {
-        console.log("TWEETED: " + text);
-    }
-};
-
 var app = {
 
     initialize: function() {
