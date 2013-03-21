brewmook/BeerTap

commit 7080c4868a8af70ff74b33a868668360e2271a2e

Removed fake twitter interactions

www/js/Twitter.js
@@ -21,38 +21,25 @@ Twitter.prototype.authorised = function()
 
 Twitter.prototype.getUserTimeline = function(screenName, callback)
 {
-    if (this.authorised())
-    {
-        $.getJSON("https://api.twitter.com/1/statuses/user_timeline.json?screen_name="+screenName+"&trim_user=true&include_rts=false&count=100",
-                  callback);
-    }
-    else
-    {
-        console.log('### Twitter not authorised, faking it!');
-        $.mobile.loading( 'show' );
-        setTimeout(function() { $.getJSON("js/sample.json", callback); }, 1000);
-    }
+    $.getJSON("https://api.twitter.com/1/statuses/user_timeline.json?screen_name="+screenName+"&trim_user=true&include_rts=false&count=100",
+              callback);
 };
 
 Twitter.prototype.tweet = function(text, success, failure)
 {
-    var logMessage = "### TWEETED as " + this.store.screenName + ":\n" + text;
     if (this.authorised())
     {
         this.oAuth.post("https://api.twitter.com/1.1/statuses/update.json",
                         { status: text },
-                        function(data) { console.log(logMessage); success(data); },
+                        function(data) {
+                            console.log("### TWEETED as " + this.store.screenName + ":\n" + text);
+                            success(data);
+                        },
                         failure);
     }
     else
     {
-        console.log('### Twitter not authorised, faking it!');
-        $.mobile.loading('show');
-        setTimeout(function() {
-            $.mobile.loading('hide');
-            console.log(logMessage);
-            success('FAKE');
-        }, 1000);
+        failure("Twitter not authorised");
     }
 };
 

commit d5d4091e913e116578a952d0c41f27d5db408bd7

Fix bug that crept previously
7080c4868a8af70ff74b33a868668360e2271a2e introduced this bug - moved a
'this' inside the closure so store was undefined.

www/js/Twitter.js
@@ -31,10 +31,11 @@ Twitter.prototype.tweet = function(text, success, failure)
 {
     if (this.authorised())
     {
+        var screenName = this.store.screenName;
         this.oAuth.post("https://api.twitter.com/1.1/statuses/update.json",
                         { status: text },
                         function(data) {
-                            console.log("### TWEETED as " + this.store.screenName + ":\n" + text);
+                            console.log("### TWEETED as " + screenName + ":\n" + text);
                             success(data);
                         },
                         failure);