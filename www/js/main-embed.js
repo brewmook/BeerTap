0d1b7b848d75f99bacd532028fa3b3bd2210d9a2
Dave E <dave.git.2012@coolhandmook.com>
2013-03-24 04:08:27 +0000

    Remove jquery mobile again, custom css instead

--- www/js/main-embed.js
+++ www/js/main-embed.js
@@ -5,13 +5,13 @@ requirejs.config({
     }
 });
 
-requirejs(['lib/domReady!', 'TwitterProxy', 'JQMListView', 'ListPage'],
-function (doc, TwitterProxy, JQMListView, ListPage) {
+requirejs(['lib/domReady!', 'TwitterProxy', 'ListView', 'ListPage'],
+function (doc, TwitterProxy, ListView, ListPage) {
     
     var twitter = new TwitterProxy("proxy/twitter_user_timeline_json.php");
-    var factory = {newListView:function(x,y){return new JQMListView(x,y);}};
+    var factory = {newListView:function(x,y){return new ListView(x,y);}};
     var view = new ListPage('BeerTapDemo', twitter, factory);
     view.view.page.appendTo('body');
-    $.mobile.changePage("#BeerTapDemo");
+    view.model.load('BeerTapDemo');
 
 });
