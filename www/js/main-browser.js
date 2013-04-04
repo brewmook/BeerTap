85d3e7676957210e4bffa8b30eb258705a715cc6
Dave E <dave.git.2012@coolhandmook.com>
2013-04-04 21:55:51 +0100

    Regular expression fix to find base url

--- www/js/main-browser.js
+++ www/js/main-browser.js
@@ -5,7 +5,7 @@ requirejs.config({
 
 requirejs(['lib/domReady!', 'BeerTap'],
 function (doc, BeerTap) {
-    var match = /(.*)\/[^\/]+/.exec(window.location.href);
+    var match = /(.*:\/\/[^?#]+\/)/.exec(window.location.href);
     if (match)
     {
         var home = match[1];
@@ -15,12 +15,12 @@ function (doc, BeerTap) {
             if (uri.host == "api.twitter.com")
             {
                 var paths = {
-                    "/1/statuses/user_timeline.json" :   "/proxy/twitter_1_statuses_user_timeline.php",
-                    "/1.1/statuses/user_timeline.json" : "/proxy/twitter_1_1_statuses_user_timeline.php",
-                    "/1.1/statuses/update.json" :        "/proxy/twitter_1_1_statuses_update.php",
-                    "/oauth/request_token" :             "/proxy/twitter_oauth_request_token.php",
-                    "/oauth/authorize" :                 "/proxy/twitter_oauth_authorize.php",
-                    "/oauth/access_token" :              "/proxy/twitter_oauth_access_token.php"
+                    "/1/statuses/user_timeline.json" :   "proxy/twitter_1_statuses_user_timeline.php",
+                    "/1.1/statuses/user_timeline.json" : "proxy/twitter_1_1_statuses_user_timeline.php",
+                    "/1.1/statuses/update.json" :        "proxy/twitter_1_1_statuses_update.php",
+                    "/oauth/request_token" :             "proxy/twitter_oauth_request_token.php",
+                    "/oauth/authorize" :                 "proxy/twitter_oauth_authorize.php",
+                    "/oauth/access_token" :              "proxy/twitter_oauth_access_token.php"
                 };
                 result = [home, paths[uri.path], '?', uri.query].join('');
             }
