7873a0fa53b4037412bd0ad95e50476dabe07888
Dave E <dave.git.2012@coolhandmook.com>
2013-04-13 10:41:55 +0100

    Basic build scripts to inject version info

---
+++ Version.py
@@ -0,0 +1,19 @@
+import os
+from datetime import datetime
+
+Version = {
+    'major':     0,
+    'minor':     1,
+    'patch':     3,
+    'revision':  os.popen("git rev-parse HEAD").read().strip(),
+    'timestamp': int(datetime.utcnow().strftime("%s")) * 1000,
+    }
+
+def processTemplate(source, target):
+    with open(source, 'r') as f:
+        content = f.read()
+    with open(target, 'w') as f:
+        f.write(content.format(**Version))
+
+processTemplate('templates/Version.js', 'www/js/Version.js')
+processTemplate('templates/config.xml', 'www/config.xml')
---
+++ templates/Version.js
@@ -0,0 +1,12 @@
+define(function() {{
+
+    return {{
+        major:     {major},
+        minor:     {minor},
+        patch:     {patch},
+        string:    "{major}.{minor}.{patch}",
+        revision:  "{revision}",
+        timestamp: {timestamp}
+    }};
+
+}});
