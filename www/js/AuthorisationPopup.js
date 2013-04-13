97956dceb14355cb9d391e37778d6edef2b6afc9
Dave E <dave.git.2012@coolhandmook.com>
2013-04-13 10:24:32 +0100

    id isn't required

--- www/js/AuthorisationPopup.js
+++ www/js/AuthorisationPopup.js
@@ -3,12 +3,12 @@ define(function() {
     function AuthorisationPopup(parentPage)
     {
         this.popup =
-        $('<div data-role="popup" id="" data-theme="d" data-overlay-theme="b" class="ui-content" style="min-width:250px;max-width:250px;">\
+        $('<div data-role="popup" data-theme="d" data-overlay-theme="b" class="ui-content" style="min-width:250px;max-width:250px;">\
              <h3></h3>\
              <p></p>\
              <a href="#" data-rel="back" data-role="button" data-icon="check">Ok</a>\
            </div>')
-           .appendTo(parentPage);
+            .appendTo(parentPage);
     }
     
     AuthorisationPopup.prototype.show = function(heading, text, okEnabled)
